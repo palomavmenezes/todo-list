d3594421a470165a6c1527ca754c9b59430dfe4d
palomavmenezes <palomacg.contato@gmail.com>
2023-12-18 12:45:29 -0300

    Code Review - Realizando a limpeza do código

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,12 +1,13 @@
 // src/app/components/Header.tsx
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer: React.FC = () => {
   return (
     <footer>
       <div className='mx-auto h-10 flex flex-shrink-0 items-center justify-center bg-purple-700'>
         <p className='text-white'>
-          Copyright 2023 -  Desenvolvido por Paloma Menezes
+          Copyright 2023 -  Desenvolvido por <Link to="https://www.linkedin.com/in/palomamenezes/" target='_blank'>Paloma Menezes</Link>
         </p>
       </div>
     </footer>
